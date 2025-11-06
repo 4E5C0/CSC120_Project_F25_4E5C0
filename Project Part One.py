e192e64bd8d29623c32202279f894f5a164d6a79
Ti <TilanW745@gmail.com>
2025-11-06 15:37:10 -0500

    Commit

---
+++ Project Part One.py
@@ -0,0 +1,9 @@
+game_name = "Panama Man"
+print("Welcome to " + game_name + "!")
+print("======================")
+
+
+print("Before we begin, what is your character's name?")
+name = input("Enter your name: ")
+
+print("Great, " + name + "! Let's begin the adventure!")
